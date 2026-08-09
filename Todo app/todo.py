7d2060cf7094b769e46aae84aabdb86fec9599ea
ashishsrs01 <ashishs.r.s.01@gmail.com>
2026-08-09 10:55:46 +0000

    refactor: updated task addition in new text file for more flexibility.

--- Todo app/todo.py
+++ Todo app/todo.py
@@ -1,36 +1,38 @@
 def todo():
 
-    tasks = []
-
+   
     def show_menu():
-        print('-- Todo Menu --')
+        print('\n-- Todo Menu --')
         print('Press 1 to view tasks')
         print('Press 2 to add task ')
         print('Press 3 to delete task ')
         print('Press 4 to Exit')
 
     def view_task():
-
-        if not tasks:
-            print('NO task exist yet!')
-        else:
-            print(tasks)
+         with open('Todo app/task.txt', 'r') as f:
+             task = f.readlines()
+             for task in task:
+                print(task.strip())
 
     def add_task():
         print('Enter the task to add: ')
-        task = input()
-        tasks.append(task)
+        t = input()
+        with open('Todo app/task.txt', 'a') as f:
+            f.writelines(t + '\n')
         print('Task added successfully!')
 
     def delete_task():
         print('Enter the task for deletion: ')
-        task = input()
-        if task in tasks:
-            tasks.remove(task)
-            print('Taks deleted successfully')
-        else:
-            print('this task not existed')
-        
+        t = input()
+        with open('Todo app/task.txt', 'r') as f:
+            task = f.readlines()
+
+            if t in task:
+                f.remove(t)
+                print('Task deleted successfully!')
+            else:
+                print('Task not existed')
+
 
     while True:
 
