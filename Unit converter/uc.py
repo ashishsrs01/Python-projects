cf434dd43813c94ee0d6be25ece421b233bd4d7f
ashishsrs01 <ashishs.r.s.01@gmail.com>
2026-08-10 08:58:56 +0000

    feat: enhance start function with unit type selection and error handling

--- Unit converter/uc.py
+++ Unit converter/uc.py
@@ -1,17 +1,34 @@
 def start():
-    v = float(input("\nEnter the value: "))
-    b = input('Enter the base unit of value: ')
-    t = input('Enter the target unit of value: ')
+
+    try:
+        print('\nChoose your unit type\n')
+        print('1 -> Length/Distance')
+        print('2 -> Mass/Weight')
+        print('3 -> Temperature')
+        print('4 -> Time')
+        print('5 -> Speed')
+        print('6 -> Area')
+        print('7 -> Volume')
+        print('8 -> Energy')
+        print('9 -> Pressure')
+        print('10 -> Data/Storage\n')
+        n = int(input())
+    except ValueError:
+        print('Invalid input LOL!')
+    except Exception as e:
+        print(f'\nUnexpected Error occurred, {e}')
+    
 
 
 def Menu():
-    print('--- Welcome to unit converter ---')
+    print('--- Welcome to Unit Converter ---')
     print('\nEnter 0 to start')
     n = int(input())
 
     if n == 0:
         start()
-    else: 
+    else:
+        print('Not valid') 
         return
 
 
