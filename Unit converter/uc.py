1332e808b64bdb04480fe1125d189415ad6183f9
ashishsrs01 <ashishs.r.s.01@gmail.com>
2026-08-10 08:09:13 +0000

    feat: implement start function and menu for unit converter

--- Unit converter/uc.py
+++ Unit converter/uc.py
@@ -1,3 +1,17 @@
+def start():
+    v = float(input("\nEnter the value: "))
+    b = input('Enter the base unit of value: ')
+    t = input('Enter the target unit of value: ')
 
+
+def Menu():
+    print('--- Welcome to unit converter ---')
+    print('\nEnter 0 to start')
+    n = int(input())
+
+    if n == 0:
+        start()
+    else: 
+        return
 
 
